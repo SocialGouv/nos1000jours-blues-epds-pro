a1b6a92d455285acd1ac51989a4c134ee8e5298a
Audrey Lebret <alebret@ippon.fr>
2021-08-19 10:01:20 +0200

    feat(résultat): Ajout de la conf env et requête

--- apollo-client.js
+++ apollo-client.js
@@ -1,8 +1,66 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
-const client = new ApolloClient({
-    uri: "https://backoffice-preprod-les1000jours.dev.fabrique.social.gouv.fr/graphql",
+export const client = new ApolloClient({
+    uri: `${process.env.API_URL}/graphql?nocache`,
     cache: new InMemoryCache(),
+    headers: { "content-type": "application/json" },
 });
 
-export default client;
+export const QUESTIONNAIRE_EPDS = gql`
+    query QuestionnaireEpds {
+        questionnaireEpds {
+        libelle,
+        ordre,
+        locale,
+        reponse_1_libelle,
+        reponse_1_points,
+        reponse_2_libelle,
+        reponse_2_points,
+        reponse_3_libelle,
+        reponse_3_points,
+        reponse_4_libelle,
+        reponse_4_points,
+    }
+}`;
+
+export const EPDS_ADD_RESPONSE = gql`
+  mutation (
+    $genre: ENUM_REPONSESEPDS_GENRE!
+    $compteur: Int!
+    $score: Int!
+    $reponseNum1: Int!
+    $reponseNum2: Int!
+    $reponseNum3: Int!
+    $reponseNum4: Int!
+    $reponseNum5: Int!
+    $reponseNum6: Int!
+    $reponseNum7: Int!
+    $reponseNum8: Int!
+    $reponseNum9: Int!
+    $reponseNum10: Int!
+  ) {
+    createReponsesEpd(
+      input: {
+        data: {
+          genre: $genre
+          compteur: $compteur
+          score: $score
+          reponse_1: $reponseNum1
+          reponse_2: $reponseNum2
+          reponse_3: $reponseNum3
+          reponse_4: $reponseNum4
+          reponse_5: $reponseNum5
+          reponse_6: $reponseNum6
+          reponse_7: $reponseNum7
+          reponse_8: $reponseNum8
+          reponse_9: $reponseNum9
+          reponse_10: $reponseNum10
+        }
+      }
+    ) {
+      reponsesEpd {
+        id
+        created_at
+      }
+    }
+  }`;
