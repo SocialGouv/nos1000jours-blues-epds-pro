f5fb26fe33a3582875e6d63e0a5ede8935b43972
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-09-17 17:54:36 +0200

    fix: Add API_URL logs, again

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import { API_URL } from "./src/constants/constants";
-console.log("API_URL", API_URL);
-console.log("process.env.NEXT_PUBLIC_API_URL", process.env.NEXT_PUBLIC_API_URL);
+console.log("APOLLO API_URL", API_URL);
+console.log("APOLLO process.env.NEXT_PUBLIC_API_URL", process.env.NEXT_PUBLIC_API_URL);
 export const client = new ApolloClient({
   uri: `${API_URL}/graphql?nocache`,
   cache: new InMemoryCache(),
